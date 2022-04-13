eaa850d77a3ee9c61568eb85356e6e8c8007f8c1
gabrielfernando01 <gabriel.rozepeda@gmail.com>
2022-04-13 00:00:41 -0500

    update readme and file .py

--- loops/loops_scripts/if_else.py
+++ loops/loops_scripts/if_else.py
@@ -1,3 +1,19 @@
 # =====================================================================================================
 # Conditional Statements in Pyhton
 
+# Python: It's All about the identation
+if 'foo' in ['bar', 'mex', 'can']:
+    print("Hola mundo")
+    print("Hello world")
+print("print nothing")
+
+# The else and elif clauses
+x = 20
+
+if x < 50:
+    print('(first suite)')
+    print('x is smile')
+else:
+    print('(second suite)')
+    print('x is large')
+    
