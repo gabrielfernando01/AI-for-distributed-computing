607c004ae89d952c50500c822a48351938913a8e
gabrielfernando01 <fernando.rozepeda@gmail.com>
2022-05-06 10:20:17 -0500

    add for and function files

---
+++ basic/for.py
@@ -0,0 +1,28 @@
+# El bucle for en Python
+# for se utiliza para recorrer los elementos de un objeto iterable (lista,
+# tupla, conjunto, diccionario, ...) y ejecutar un bloque de código.
+# Su sintaxis es la siguiente:
+for <elem> in <iterable>:
+    <Your code>
+
+# Para imprimir una lista de números en la consola.
+num = [5, 3, 2, 100, 2]
+for n in num:
+    print(n)
+
+# Un iterable es un objeto que se puede iterar sobre él, ie, que permite
+# recorrer sus elementos uno a uno. Un objeto iterable es aquél que puede
+# pasarse como parámetro de la función iter().
+num = [4, 3, 7, 0, 13]
+it = iter(num)
+next(it)
+next(it)
+
+# En Python, los tipos principales: list, tuple, dict, set o string entre
+# otros, son iterables, por lo que podrán ser usados en el bucle for.
+
+# Bucle en diccionarios.
+
+
+
+
--- basic/function.py
+++ basic/function.py
@@ -14,6 +14,48 @@ print('End')
 
 # Sentencia return
 # return que no devuelve ningún valor
+def cuadrado_num_par(number):
+    if not number % 2 == 0:
+        return
+    else:
+        print(number ** 2)
+
+cuadrado_num_par(8)
+cuadrado_num_par(7)
+
+# Varios return en una misma función
+def is_even(number):
+    if number % 2 == 0:
+        return True
+    else:
+        return False
+
+is_even(12)
+is_even(31)
+
+# Devolver más de un valor con return en Python.
+def cuadrado_cubo(number):
+    return number ** 2, number ** 3
+
+sqrt, cube = cuadrado_cubo(3)
+sqrt
+cube
+
+# Se puede devolver los diferentes resultados en una lista.
+def multiples_of(number):
+    result = []
+    for i in range(11):
+        result.append(number * i)
+    return result
+
+res = multiples_of(11)
+res
+
+
+
+
+
+
 
 
 
